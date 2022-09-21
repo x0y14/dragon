3d5e985d7f868d3aae7f37d911d5b1bd48729360
x0y14 <ya01ma08@gmail.com>
2022-09-21 17:40:16 +0900

    フィールド？を追加 new関数を追加

--- pkg/tokenizer/position.go
+++ pkg/tokenizer/position.go
@@ -1,4 +1,15 @@
 package tokenizer
 
 type Position struct {
+	LineNo  int
+	LineAt  int
+	WholeAt int
+}
+
+func NewPosition(lno, lat, wat int) *Position {
+	return &Position{
+		LineNo:  lno,
+		LineAt:  lat,
+		WholeAt: wat,
+	}
 }
