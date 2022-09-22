5970a3662a498bcaebd57661741483ba2948dff8
x0y14 <ya01ma08@gmail.com>
2022-09-22 16:47:52 +0900

    clone関数と、デバッグ用の関数を追加

--- pkg/tokenizer/position.go
+++ pkg/tokenizer/position.go
@@ -1,6 +1,8 @@
 package tokenizer
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Position struct {
 	LineNo  int
@@ -18,4 +20,28 @@ func NewPosition(lno, lat, wat int) *Position {
 
 func (p *Position) String() string {
 	return fmt.Sprintf("%s:%s", p.LineNo, p.LineAt)
+}
+
+// NewPositionFromString 入力の内容を読み取った後の時の位置を返す, Debug用
+func NewPositionFromString(s string) *Position {
+	p := &Position{
+		LineNo:  1,
+		LineAt:  0,
+		WholeAt: 0,
+	}
+	for _, r := range []rune(s) {
+		if r == '\n' {
+			p.LineNo++
+			p.WholeAt++
+			p.LineAt = 0
+			continue
+		}
+		p.WholeAt++
+		p.LineAt++
+	}
+	return p
+}
+
+func (p *Position) Clone() *Position {
+	return &Position{LineNo: p.LineNo, LineAt: p.LineAt, WholeAt: p.WholeAt}
 }
