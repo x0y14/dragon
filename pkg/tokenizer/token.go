6da79090084c902641e7f8b34c975ce6ec68adcd
x0y14 <ya01ma08@gmail.com>
2022-09-22 15:08:28 +0900

    New関数を追加

--- pkg/tokenizer/token.go
+++ pkg/tokenizer/token.go
@@ -1,9 +1,30 @@
 package tokenizer
 
 type Token struct {
-	Pos  *Position
 	Kind interface {
 		int
 		String() string
+	}
+	Pos *Position
+
+	// 即値格納用フィールド
+	S string
+	I int64
+	F float64
+
+	Next *Token
+}
+
+func NewToken(kind interface {
+	int
+	String() string
+}, pos *Position, s string, f float64, i int64) *Token {
+	return &Token{
+		Pos:  pos,
+		Kind: kind,
+		S:    s,
+		I:    i,
+		F:    f,
+		Next: nil,
 	}
 }
