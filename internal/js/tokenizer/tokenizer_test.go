f79180938025d84dfadcf4db4dad05500d169c47
x0y14 <ya01ma08@gmail.com>
2022-09-22 16:50:31 +0900

    ・testifyでは詳しい差分を表示できなかったのでgo-cmpで詳しい差分を表示できるようにした。 ・期待値を全て手書きしてテストを通るように

--- internal/js/tokenizer/tokenizer_test.go
+++ internal/js/tokenizer/tokenizer_test.go
@@ -2,7 +2,7 @@ package tokenizer
 
 import (
 	"dragon/pkg/tokenizer"
-	"github.com/stretchr/testify/assert"
+	"github.com/google/go-cmp/cmp"
 	"testing"
 )
 
@@ -16,16 +16,261 @@ func TestTokenizer_Tokenize(t *testing.T) {
 		{ // 出典: https://www.programiz.com/javascript/examples/add-number
 			"add 2 nums",
 			`
-				const num1 = 5;
-				const num2 = 3;
-				
-				// add two numbers
-				const sum = num1 + num2;
-				
-				// display the sum
-				console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);`,
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);`,
 			nil,
-			nil,
+			&tokenizer.Token{
+				Kind: KWConst,
+				Pos:  tokenizer.NewPositionFromString("\n"),
+				S:    "const",
+				I:    0,
+				F:    0,
+				Next: &tokenizer.Token{
+					Kind: Identifier,
+					Pos:  tokenizer.NewPositionFromString("\nconst "),
+					S:    "num1",
+					I:    0,
+					F:    0,
+					Next: &tokenizer.Token{
+						Kind: OPAssign,
+						Pos:  tokenizer.NewPositionFromString("\nconst num1 "),
+						S:    "",
+						I:    0,
+						F:    0,
+						Next: &tokenizer.Token{
+							Kind: Integer,
+							Pos:  tokenizer.NewPositionFromString("\nconst num1 = "),
+							S:    "",
+							I:    5,
+							F:    0,
+							Next: &tokenizer.Token{
+								Kind: SYSemi,
+								Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5"),
+								S:    "",
+								I:    0,
+								F:    0,
+								Next: &tokenizer.Token{
+									Kind: KWConst,
+									Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\n"),
+									S:    "const",
+									I:    0,
+									F:    0,
+									Next: &tokenizer.Token{
+										Kind: Identifier,
+										Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst "),
+										S:    "num2",
+										I:    0,
+										F:    0,
+										Next: &tokenizer.Token{
+											Kind: OPAssign,
+											Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 "),
+											S:    "",
+											I:    0,
+											F:    0,
+											Next: &tokenizer.Token{
+												Kind: Integer,
+												Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = "),
+												S:    "",
+												I:    3,
+												F:    0,
+												Next: &tokenizer.Token{
+													Kind: SYSemi,
+													Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3"),
+													S:    "",
+													I:    0,
+													F:    0,
+													Next: &tokenizer.Token{
+														Kind: KWConst,
+														Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\n"),
+														S:    "const",
+														I:    0,
+														F:    0,
+														Next: &tokenizer.Token{
+															Kind: Identifier,
+															Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst "),
+															S:    "sum",
+															I:    0,
+															F:    0,
+															Next: &tokenizer.Token{
+																Kind: OPAssign,
+																Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum "),
+																S:    "",
+																I:    0,
+																F:    0,
+																Next: &tokenizer.Token{
+																	Kind: Identifier,
+																	Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = "),
+																	S:    "num1",
+																	I:    0,
+																	F:    0,
+																	Next: &tokenizer.Token{
+																		Kind: OPAdd,
+																		Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 "),
+																		S:    "",
+																		I:    0,
+																		F:    0,
+																		Next: &tokenizer.Token{
+																			Kind: Identifier,
+																			Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + "),
+																			S:    "num2",
+																			I:    0,
+																			F:    0,
+																			Next: &tokenizer.Token{
+																				Kind: SYSemi,
+																				Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2"),
+																				S:    "",
+																				I:    0,
+																				F:    0,
+																				Next: &tokenizer.Token{
+																					Kind: Identifier,
+																					Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\n"),
+																					S:    "console",
+																					I:    0,
+																					F:    0,
+																					Next: &tokenizer.Token{
+																						Kind: SYDot,
+																						Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole"),
+																						S:    "",
+																						I:    0,
+																						F:    0,
+																						Next: &tokenizer.Token{
+																							Kind: Identifier,
+																							Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole."),
+																							S:    "log",
+																							I:    0,
+																							F:    0,
+																							Next: &tokenizer.Token{
+																								Kind: SYLrb,
+																								Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log"),
+																								S:    "",
+																								I:    0,
+																								F:    0,
+																								Next: &tokenizer.Token{
+																									Kind: String,
+																									Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log("),
+																									S:    "The sum of ",
+																									I:    0,
+																									F:    0,
+																									Next: &tokenizer.Token{
+																										Kind: OPAdd,
+																										Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' "),
+																										S:    "",
+																										I:    0,
+																										F:    0,
+																										Next: &tokenizer.Token{
+																											Kind: Identifier,
+																											Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + "),
+																											S:    "num1",
+																											I:    0,
+																											F:    0,
+																											Next: &tokenizer.Token{
+																												Kind: OPAdd,
+																												Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 "),
+																												S:    "",
+																												I:    0,
+																												F:    0,
+																												Next: &tokenizer.Token{
+																													Kind: String,
+																													Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 + "),
+																													S:    " and ",
+																													I:    0,
+																													F:    0,
+																													Next: &tokenizer.Token{
+																														Kind: OPAdd,
+																														Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 + ' and ' "),
+																														S:    "",
+																														I:    0,
+																														F:    0,
+																														Next: &tokenizer.Token{
+																															Kind: Identifier,
+																															Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 + ' and ' + "),
+																															S:    "num2",
+																															I:    0,
+																															F:    0,
+																															Next: &tokenizer.Token{
+																																Kind: OPAdd,
+																																Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 + ' and ' + num2 "),
+																																S:    "",
+																																I:    0,
+																																F:    0,
+																																Next: &tokenizer.Token{
+																																	Kind: String,
+																																	Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 + ' and ' + num2 + "),
+																																	S:    " is: ",
+																																	I:    0,
+																																	F:    0,
+																																	Next: &tokenizer.Token{
+																																		Kind: OPAdd,
+																																		Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' "),
+																																		S:    "",
+																																		I:    0,
+																																		F:    0,
+																																		Next: &tokenizer.Token{
+																																			Kind: Identifier,
+																																			Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + "),
+																																			S:    "sum",
+																																			I:    0,
+																																			F:    0,
+																																			Next: &tokenizer.Token{
+																																				Kind: SYRrb,
+																																				Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum"),
+																																				S:    "",
+																																				I:    0,
+																																				F:    0,
+																																				Next: &tokenizer.Token{
+																																					Kind: SYSemi,
+																																					Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum)"),
+																																					S:    "",
+																																					I:    0,
+																																					F:    0,
+																																					Next: &tokenizer.Token{
+																																						Kind: Eof,
+																																						Pos:  tokenizer.NewPositionFromString("\nconst num1 = 5;\nconst num2 = 3;\n\n// add two numbers\nconst sum = num1 + num2;\n\n// display the sum\nconsole.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);"),
+																																						S:    "",
+																																						I:    0,
+																																						F:    0,
+																																						Next: nil,
+																																					},
+																																				},
+																																			},
+																																		},
+																																	},
+																																},
+																															},
+																														},
+																													},
+																												},
+																											},
+																										},
+																									},
+																								},
+																							},
+																						},
+																					},
+																				},
+																			},
+																		},
+																	},
+																},
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 
@@ -33,10 +278,13 @@ func TestTokenizer_Tokenize(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			tokenizer_ := NewTokenizer()
 			tok, err := tokenizer_.Tokenize([]rune(tt.in))
-			if !assert.Equal(t, tt.expectErr, err) {
-				t.Fatalf("%v", err)
+			if diff := cmp.Diff(tt.expectErr, err); diff != "" {
+				t.Errorf("Tokenize() Err mismatch (-want +got):\n%s", diff)
 			}
-			assert.Equal(t, tt.expectToken, tok)
+
+			if diff := cmp.Diff(tt.expectToken, tok); diff != "" {
+				t.Errorf("Tokenize() Token mismatch (-want +got):\n%s", diff)
+			}
 		})
 	}
 }
