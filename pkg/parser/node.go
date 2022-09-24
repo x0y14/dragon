83e0cee830801fb20eee4b8387d74c73449bd68c
x0y14 <ya01ma08@gmail.com>
2022-09-24 17:17:25 +0900

    node追加

--- pkg/parser/node.go
+++ pkg/parser/node.go
@@ -1,11 +1,36 @@
 package parser
 
-import "dragon/pkg/tokenizer"
+type NodeKind interface {
+	String() string
+}
 
 type Node struct {
-	Pos  *tokenizer.Position
-	Kind interface {
-		int
-		String() string
+	Kind     NodeKind
+	LHS      *Node
+	RHS      *Node
+	Child    *Node
+	Children []*Node
+
+	S string
+	F float64
+	I int64
+	B bool
+}
+
+func (n *Node) Clone() *Node {
+	return NewNode(n.Kind, n.LHS, n.RHS, n.Child, n.Children, n.S, n.F, n.I, n.B)
+}
+
+func NewNode(kind NodeKind, lhs, rhs, child *Node, children []*Node, s string, f float64, i int64, b bool) *Node {
+	return &Node{
+		Kind:     kind,
+		LHS:      lhs,
+		RHS:      rhs,
+		Child:    child,
+		Children: children,
+		S:        s,
+		F:        f,
+		I:        i,
+		B:        b,
 	}
 }
